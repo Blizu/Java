bf508c865a403a295e2ff50a12dacfe79398d998
piotrb <piotr.blizewski@gmail.cok>
2020-03-26 11:11:15 +0100

    TPO_2: Implement getRateFor method

--- PPJ/Internetowe2/src/TPO2/Main.java
+++ PPJ/Internetowe2/src/TPO2/Main.java
@@ -1,27 +1,179 @@
-package TPO2;
-
 /**
  *
  *  @author Bliżewski Piotr S16710
  *
  */
 
+package TPO2;
 
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.json.JSONObject;
 
-public class Main {
+
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+import javax.security.auth.login.Configuration;
+import java.util.Currency;
+import java.util.Locale;
+import java.util.Map;
+
+
+public class Main extends Application {
+
+    private static String wikipedia;
+    private static String weatherJson;
+    private static Double rate1;
+    private static Double rate2;
+
     public static void main(String[] args) {
-        Service s = new Service("Uganda");
-        String weatherJson = s.getWeather("Kampala");
+
+
+
+        Service s = new Service("Canada");
+        weatherJson = s.getWeather("Toronto");
+        rate1 = s.getRateFor("USD");
+        rate2 = s.getNBPRate();
+        wikipedia = s.getDescriptionFromWikipedia("Warsaw");
+
+
         System.out.println(weatherJson);
-        Double rate1 = s.getRateFor("EUR");
         System.out.println(rate1);
-        Double rate2 = s.getNBPRate();
         System.out.println(rate2);
-
-
-
-        // ...
-        // część uruchamiająca GUI
+       //launch(args);
     }
+
+    private Button Pogoda, Kurs_Waluty, Kurs_NBP, Opis, Exit;
+    private TextArea Wyszukiwarka;
+    private Scene Scena;
+    private Pane kontener;
+    private WebView browser;
+    private WebEngine engine;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Scena(primaryStage);
+        primaryStage.setTitle("Pogoda");
+        primaryStage.setScene(Scena);
+        primaryStage.show();
+    }
+
+    private void Scena (Stage primaryStage){
+
+        Pogoda = new Button("Pokaż pogodę");
+        Pogoda.setOnAction((event) ->{
+            if(weatherJson != null){
+                show_it(formatWeather(weatherJson));
+            }
+        });
+
+        Kurs_Waluty = new Button("Pokaż kurs waluty");
+        Kurs_Waluty.setOnAction((event ->{
+            if(rate1 != null){
+                show_it("Kurs Waluty:\n" + rate1);
+            }
+        }));
+
+        Kurs_NBP = new Button("Pokaż kurs NBP");
+        Kurs_NBP.setOnAction((event ->{
+            if(rate2 != null){
+                show_it("Kurs NBP: \n" + rate2);
+            }
+        }));
+
+        Opis = new Button("Opis miasta");
+        Opis.setOnAction((event ->{
+            if(wikipedia != null){
+                engine.load(wikipedia);
+            }
+        }));
+
+        Exit = new Button("Wyjście");
+        Exit.setOnAction((event ->{
+            System.exit(0);
+            System.out.println("Poprawnie zamknięto program");
+        }));
+
+        Wyszukiwarka = new TextArea();
+
+        kontener = new Pane();
+        kontener.setPadding(new Insets(20, 20, 20, 20));
+
+        Pogoda.setLayoutX(20);
+        Pogoda.setLayoutY(20);
+        Pogoda.setMinSize(160, 50);
+
+        Kurs_Waluty.setLayoutX(180);
+        Kurs_Waluty.setLayoutY(20);
+        Kurs_Waluty.setMinSize(160, 50);
+
+        Kurs_NBP.setLayoutX(340);
+        Kurs_NBP.setLayoutY(20);
+        Kurs_NBP.setMinSize(160, 50);
+
+        Opis.setLayoutX(500);
+        Opis.setLayoutY(20);
+        Opis.setMinSize(160, 50);
+
+        Exit.setLayoutX(660);
+        Exit.setLayoutY(20);
+        Exit.setMinSize(160, 50);
+
+        Wyszukiwarka.setLayoutX(20);
+        Wyszukiwarka.setLayoutY(90);
+        Wyszukiwarka.setMinSize(800, 10);
+
+        kontener.getChildren().add(Pogoda);
+        kontener.getChildren().add(Kurs_Waluty);
+        kontener.getChildren().add(Kurs_NBP);
+        kontener.getChildren().add(Opis);
+        kontener.getChildren().add(Exit);
+        kontener.getChildren().add(Wyszukiwarka);
+
+        browser = new WebView();
+        engine = browser.getEngine();
+
+        kontener.getChildren().add(browser);
+        browser.setLayoutX(20);
+        browser.setLayoutY(290);
+
+        Scena = new Scene(kontener, 840, 910);
+
+    }
+
+    public void show_it(String text){
+        Wyszukiwarka.clear();
+        Wyszukiwarka.appendText(text);
+    }
+
+    public String formatWeather(String raw_text){
+        String formatted_text ="";
+
+        try {
+            JSONObject obj = new JSONObject(raw_text);
+
+            if(!obj.isNull("main")){
+                formatted_text += "Miasto: " + obj.getString("name") +"\n";
+                formatted_text += "Kraj: " + obj.getJSONObject("sys").get("country") + "\n";
+                formatted_text += "Temperatura: " + obj.getJSONObject("main").get("temp") + "\n";
+                formatted_text += "Temperatura min: " + obj.getJSONObject("main").get("temp_min") + "\n";
+                formatted_text += "Temperatura max: " + obj.getJSONObject("main").get("temp_max") + "\n";
+                formatted_text += "Ciśnienie: " + obj.getJSONObject("main").get("pressure") + "\n";
+                formatted_text += "Wilgotność: " + obj.getJSONObject("main").get("humidity") + "\n";
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return formatted_text;
+    }
+
 }
--- PPJ/Internetowe2/src/TPO2/Service.java
+++ PPJ/Internetowe2/src/TPO2/Service.java
@@ -1,12 +1,17 @@
 package TPO2;
 
 import javafx.util.Pair;
+
+import javax.security.auth.login.Configuration;
 import java.net.*;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+import java.util.Currency;
+import java.util.Locale;
 public class Service {
 
     private String nazwaKraju;
@@ -14,10 +19,45 @@ public class Service {
 
     public Service(String string) {
             this.nazwaKraju = string;
-            this.currency = getCurrencyForCountry(nazwaKraju);
+            this.currency = getCurrencyForCountry2(nazwaKraju);
     }
 
-    public String getCurrencyForCountry(String nazwaKraju){
+
+    private Map<String, String> getAvailableCurrencies() {
+
+        Locale locale = new Locale("en_US");
+        Locale.setDefault(locale);
+
+
+        Locale[] localesTab = locale.getAvailableLocales();
+
+
+        Map<String, String> currencies = new TreeMap<>();
+        for (Locale loc : localesTab) {
+            try {
+                currencies.put(loc.getDisplayCountry(),
+                        Currency.getInstance(loc).getCurrencyCode());
+            } catch (Exception e) {
+
+            }
+        }
+        return currencies;
+    }
+
+    public String getCurrencyForCountry2(String nazwaKraju){
+
+        Map<String, String> currencies = getAvailableCurrencies();
+        for (String country : currencies.keySet()) {
+
+            if (country.equals(nazwaKraju)) {
+                return currencies.get(country);
+            }
+        }
+        return null;
+    }
+
+
+    /*public String getCurrencyForCountry(String nazwaKraju){
 
         String urlAddressCountryCode = "https://supply-xml.booking.com/hotels/xml/countries";
 
@@ -52,7 +92,7 @@ public class Service {
             e.printStackTrace();
             return null;
         }
-    }
+    }*/
 
     public Pair<String, Integer> getHttpResponse(String requestUrl) throws IOException {
         try {
@@ -110,7 +150,7 @@ public class Service {
     }
 
     public Double getRateFor(String base) {
-        String requestUrl = "https://api.exchangeratesapi.io/latest?base=" + base + "&symbols=" + this.currency;
+        String requestUrl = "https://api.exchangeratesapi.io/latest?base=" + this.currency + "&symbols=" + base;
         try {
             Pair<String, Integer> response = getHttpResponse(requestUrl);
             String responseString = response.getKey();
@@ -122,7 +162,7 @@ public class Service {
                 checkReturnCode(returnCode, responseString);
             }
 
-            Pattern p = Pattern.compile(".*" + this.currency + "\\\":([0-9]+\\.[0-9]+)}.*");
+            Pattern p = Pattern.compile(".*" + base + "\\\":([0-9]+\\.[0-9]+)}.*");
             Matcher m = p.matcher(responseString);
             boolean b = m.matches();
             String value = m.group(1);
@@ -168,8 +208,14 @@ public class Service {
             e.printStackTrace();
         }
         return null;
-
     }
+
+    public String getDescriptionFromWikipedia(String nazwaMiasta){
+        String city = nazwaMiasta;
+        String output = "https://en.wikipedia.org/wiki/"+city;
+        return output;
+    }
+
 }
 
 
